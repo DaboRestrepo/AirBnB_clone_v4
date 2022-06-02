ffd50325ab0fd0963e6157e70fa2450e227d3015
DaboRestrepo <daborestrepo@gmail.com>
2022-06-02 12:31:16 -0500

    Statrts with web dynamic part: -- Made the ask changes ✅. -- Add cache id to the link tags and the js tag ✅. -- Add the checkbox information to the html file ✅. -- Add the style information to the filters.html ✅. -- Try to start with the 1-hbnb.py file ⚠.

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const $ = window.$;
+$(document).ready(() => {
+    let checked = []
+    if ($('#checkbox').is(checked)) {
+        checked.fill($(this).data('amenity_id'))
+    } else {
+    }
+})
