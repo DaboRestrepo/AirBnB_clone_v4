0bdc15365cd6bd1f61b68075ac204d1cec607195
SharaGB <shara.garcia.betancur@gmail.com>
2022-06-03 11:51:46 -0500

    Loaded places from de front end

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,24 +1,48 @@
 $('document').ready(function () {
-    const checkedList = {};
-    const urlStatus = 'http://localhost:5001/api/v1/status/';
+  const checkedList = {};
+  const urlStatus = 'http://localhost:5001/api/v1/status/';
 
-    $('input[type="checkbox"]').change(function () {
-        if ($(this).is(':checked')) {
-            checkedList[$(this).attr('data-id')] = $(this).attr('data-name');
-        } else {
-            delete checkedList[$(this).attr('data-id')];
-        }
+  $('input[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      checkedList[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete checkedList[$(this).attr('data-id')];
+    }
 
-        $('div.amenities > h4').text(Object.values(checkedList).join(', '));
-    });
+    $('div.amenities > h4').text(Object.values(checkedList).join(', '));
+  });
 
-    $.get(urlStatus, function (data) {
-        if (data.status === 'OK') {
-            $('div#api_status').addClass('available');
-        } else {
-            $('div#api_status').removeClass('available');
-        }
-    });
+  $.get(urlStatus, function (data) {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
 
-
+  $.ajax({
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    data: '{}',
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      $.each(data, function (i, v) {
+        $(`<article>
+        <div class="title_box">
+          <h2>${v.name}</h2>
+          <div class="price_by_night">$${v.price_by_night}</div>
+        </div>
+        <div class="information">
+          <div class="max_guest">${v.max_guest} Guest</div>
+                <div class="number_rooms">${v.number_rooms} Bedroom</div>
+                <div class="number_bathrooms">${v.number_bathrooms} Bathroom</div>
+        </div>
+              <div class="description">
+          ${v.description}
+              </div>
+      </article>`).appendTo('.places');
+      });
+    }
+  });
 });
